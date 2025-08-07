858711824f8ffcbb9984098ef0b8a6f4921f6140
Saraonsy <Saraonsy64@gmail.com>
2025-08-07 15:57:56 +0300

    main.js

--- main.js
+++ main.js
@@ -1,42 +1,86 @@
+var productName = document.getElementById('productNameInput');
+var productPrice = document.getElementById('productPriceInput');
+var productCategory = document.getElementById('productCtagoryInput');
+var productDesc = document.getElementById('productDescInput');
+var productContainer = [];
 
-var productName=document.getElementById('productNameInput')
-var productPrice=document.getElementById('productPriceInput')
-var productCategory=document.getElementById('productCtagoryInput')
-var productDesc=document.getElementById('productDescInput')
+var currentIndex = -1;
 
-var productContainer =[];
-function AddProduct(){
-    var product={
-        name:productName.value,
-        price:productPrice.value,
-        category:productCategory.value,
-        desc:productDesc.value
+if (localStorage.getItem('myproducts') != null) {
+    productContainer = JSON.parse(localStorage.getItem('myproducts'));
+    displayForm();
+} else {
+    productContainer = [];
+}
 
+function AddProduct() {
+    var product = {
+        name: productName.value,
+        price: productPrice.value,
+        category: productCategory.value,
+        desc: productDesc.value
+    };
+
+    if (currentIndex === -1) {
+        productContainer.push(product);
+    } else {
+    
+        productContainer[currentIndex] = product;
+        currentIndex = -1; 
     }
-    productContainer.push(product)
-    console.log(productContainer)
+
+    localStorage.setItem('myproducts', JSON.stringify(productContainer));
+    displayForm();
     clearForm();
-    displayForm()
 }
-function clearForm(){
-    productName.value='';
-    productPrice.value='';
-    productCategory.value='';
-    productDesc.value='';
-}
-function displayForm(){
-    var cartona=``
 
-    for(var i=0; i<productContainer.length; i++){
-        cartona+= `<tr>
-        <td>${i}</td>
-        <td>${productContainer[i].name}</td>
-        <td>${productContainer[i].price}</td>
-        <td>${productContainer[i].category}</td>
-        <td>${productContainer[i].desc}</td>
-        <td> <button class="btn btn-sm btn-outline-warning">update</button></td>
-        <td> <button class="btn btn-sm btn-outline-danger">delete</button></td>
-    </tr>`
+function clearForm() {
+    productName.value = '';
+    productPrice.value = '';
+    productCategory.value = '';
+    productDesc.value = '';
+}
+
+function displayForm(list = productContainer) {
+    var cartona = ``;
+
+    for (var i = 0; i < list.length; i++) {
+        cartona += `<tr>
+            <td>${i}</td>
+            <td>${list[i].name}</td>
+            <td>${list[i].price}</td>
+            <td>${list[i].category}</td>
+            <td>${list[i].desc}</td>
+            <td> <button class="btn btn-sm btn-outline-warning" onclick="updateProduct(${i})">update</button></td>
+            <td> <button class="btn btn-sm btn-outline-danger" onclick="deleteProduct(${i})">delete</button></td>
+        </tr>`;
     }
-    document.getElementById('tableBody').innerHTML=cartona;
+
+    document.getElementById('tableBody').innerHTML = cartona;
+}
+
+function deleteProduct(index) {
+    productContainer.splice(index, 1);
+    localStorage.setItem('myproducts', JSON.stringify(productContainer)); // نحدّث localStorage
+    displayForm();
+}
+
+function updateProduct(index) {
+    var product = productContainer[index];
+    productName.value = product.name;
+    productPrice.value = product.price;
+    productCategory.value = product.category;
+    productDesc.value = product.desc;
+
+    currentIndex = index; 
+}
+
+function searchProducts(searchTerm) {
+    var searchResult = [];
+    for (var i = 0; i < productContainer.length; i++) {
+        if (productContainer[i].name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            searchResult.push(productContainer[i]);
+        }
+    }
+    displayForm(searchResult);
 }
